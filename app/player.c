2df971ebf48ec1b1f18f9ae793623158b5fde05a
Ruan Huabin <ruanhuabin@tsinghua.edu.cn>
2017-12-11 11:04:35 +0800

    first commit

---
+++ app/player.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "lcd.h"
+#include "usb.h"
+#include "media.h"
+#include "math.h"
+#include "jpg.h"
+#include "rmvb.h"
+int main()
+{
+	printf("MP3 player init...\n");
+	return 0;
+}
